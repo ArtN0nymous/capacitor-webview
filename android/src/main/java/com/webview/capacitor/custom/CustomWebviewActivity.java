52f7396837db44d7dd147634c05ab3fabea597de
ArtN0nymous <jr616934@gmail.com>
2025-06-24 18:28:19 -0600

    Implement download functionality in CustomWebViewActivity

--- android/src/main/java/com/webview/capacitor/custom/CustomWebviewActivity.java
+++ android/src/main/java/com/webview/capacitor/custom/CustomWebviewActivity.java
@@ -1,11 +1,18 @@
 package com.webview.capacitor.custom;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,9 +40,36 @@ public class CustomWebViewActivity extends AppCompatActivity {
             }
         });
 
-        webView.setDownloadListener((url1, userAgent, contentDisposition, mimetype, contentLength) -> {
-            Log.d("WebViewDownload", "Download requested: " + url1);
-            // Aquí puedes manejar la descarga (guardar archivo, usar DownloadManager, etc.)
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                Log.d("WebViewDownload", "Download requested: " + url);
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimetype);
+
+                // Agrega cookies y user agent
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+
+                // Nombre sugerido para el archivo
+                String fileName = android.webkit.URLUtil.guessFileName(url, contentDisposition, mimetype);
+                request.setTitle(fileName);
+                request.setDescription("Descargando archivo...");
+
+                // Guardar en la carpeta de descargas pública
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                // Mostrar notificación cuando termine
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+
+                // Mostrar un Toast indicando que la descarga ha comenzado
+                Toast.makeText(CustomWebViewActivity.this, "Archivo guardado en Descargas", Toast.LENGTH_LONG).show();
+            }
         });
 
         webView.loadUrl(url);
