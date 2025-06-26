0e638c715e1ced11ff61ebe0edf5582dc22a28e7
ArtN0nymous <jr616934@gmail.com>
2025-06-26 16:17:51 -0600

    Refactor CustomWebview registration to simplify plugin initialization

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { CustomWebviewPlugin } from './definitions';
 
-const CustomWebview = registerPlugin<CustomWebviewPlugin>('CustomWebview', {
-  web: () => import('./web').then((m) => new m.CustomWebviewWeb()),
-});
+const CustomWebview = registerPlugin<CustomWebviewPlugin>('CustomWebview');
 
 export * from './definitions';
 export { CustomWebview };
